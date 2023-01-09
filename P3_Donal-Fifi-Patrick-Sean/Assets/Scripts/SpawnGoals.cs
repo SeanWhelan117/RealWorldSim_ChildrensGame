d1bddddcde90fdddbabb8cc6eb8fb7a43f77d5ec
DonalHowe <58332538+DonalHowe@users.noreply.github.com>
2023-01-09 14:43:19 +0000

    added star and collisions for sling

--- P3_Donal-Fifi-Patrick-Sean/Assets/Scripts/SpawnGoals.cs
+++ P3_Donal-Fifi-Patrick-Sean/Assets/Scripts/SpawnGoals.cs
@@ -9,22 +9,28 @@ public class SpawnGoals : MonoBehaviour
     public GameObject goal;
     int randomNum = 0;
     private GameObject clone;
+    public GameObject star;
+    float Xpos=-8.0f;
+    Vector3 vec;
     // Start is called before the first frame update
     void Start()
-    {
-
+    { 
         randomNum = Random.Range(0, 3);
         clone = Instantiate(goal, spawnPoints[randomNum].position, Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    public void SpawnStar()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            Destroy(clone);
-            randomNum = Random.Range(0, 4);
-            clone =  Instantiate(goal, spawnPoints[randomNum].position, Quaternion.identity);
-        }
+     
+        vec = new Vector3(Xpos, 4.0f, 0.0f);
+        Xpos = Xpos + 1;
+        star = Instantiate(star, vec, Quaternion.identity);
+    }
+    public void changePos()
+    {
+        Destroy(clone);
+        randomNum = Random.Range(0, 4);
+        clone = Instantiate(goal, spawnPoints[randomNum].position, Quaternion.identity);
     }
 }
---
+++ P3_Donal-Fifi-Patrick-Sean/Assets/collision.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public GameObject goal;
+    public GameObject GameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("goal"))
+        {
+            Debug.Log("collided with goal");
+            GameManager.GetComponent<SpawnGoals>().changePos();
+            GameManager.GetComponent<SpawnGoals>().SpawnStar();
+        }
+    }
+}
