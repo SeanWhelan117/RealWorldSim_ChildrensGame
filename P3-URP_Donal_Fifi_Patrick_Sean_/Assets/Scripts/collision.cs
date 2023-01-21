17ef3e40735d10519f77a64ee5f08c7bd6420897
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2023-01-21 14:34:48 +0000

    Added the URP Version

---
+++ P3-URP_Donal_Fifi_Patrick_Sean_/Assets/Scripts/collision.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public GameObject goal;
+    public GameObject GameManager;
+    public int shotsHit;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameManager = GameObject.FindGameObjectWithTag("GameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("goal"))
+        {
+         
+            GameManager.GetComponent<SpawnGoals>().changePos();
+            GameManager.GetComponent<SpawnSlingPositions>().changePos();
+            //this.gameObject.GetComponent<Luggage>().ResetLuggage();
+            shotsHit++;
+           
+        }
+    }
+}
